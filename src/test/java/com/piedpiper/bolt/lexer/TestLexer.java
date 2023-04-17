5a6d278603618bcf131b09987904757e8b17ee78
EladB1 <eb86@njit.edu>
2023-04-17 13:52:23 -0400

    Refactored lexer unit tests

--- src/test/java/com/piedpiper/bolt/lexer/TestLexer.java
+++ src/test/java/com/piedpiper/bolt/lexer/TestLexer.java
@@ -9,100 +9,90 @@ import org.junit.jupiter.api.Test;
 public class TestLexer {
     private Lexer lexer = new Lexer();
 
+    void assertZeroTokens(String line) {
+        List<Token> tokens = lexer.analyzeLine(line);
+        assertEquals(0, tokens.size());
+    }
+
+    void assertOneToken(String line, Token expectedToken) {
+        List<Token> tokens = lexer.analyzeLine(line);
+        assertEquals(1, tokens.size());
+        assertEquals(expectedToken, tokens.get(0));
+
+    }
+
+    void assertManyTokens(String line, List<Token> expectedTokens) {
+        List<Token> tokens = lexer.analyzeLine(line);
+        assertEquals(expectedTokens.size(), tokens.size());
+        for (int i = 0; i < expectedTokens.size(); i++) {
+            assertEquals(expectedTokens.get(i), tokens.get(i));
+        }
+    }
+
     @Test
     void analyzeLine_shouldRecognizeInteger() {
-        List<Token> tokens = lexer.analyzeLine("10");
         Token token = new Token("NUMBER", "10");
-        assertEquals(1, tokens.size());
-        assertEquals(token.getName(), tokens.get(0).getName());
-        assertEquals(token.getValue(), tokens.get(0).getValue());
+        assertOneToken("10", token);
     }
 
     @Test
     void analyzeLine_shouldRecognizeFloat() {
-        List<Token> tokens = lexer.analyzeLine("1.0");
         Token token = new Token("NUMBER", "1.0");
-        assertEquals(1, tokens.size());
-        assertEquals(token.getName(), tokens.get(0).getName());
-        assertEquals(token.getValue(), tokens.get(0).getValue());
+        assertOneToken("1.0", token);
     }
 
     @Test
     void analyzeLine_shouldOnlyRecognizeInteger() {
-        List<Token> tokens = lexer.analyzeLine("(2)");
         Token token = new Token("NUMBER", "2");
-        assertEquals(1, tokens.size());
-        assertEquals(token.getName(), tokens.get(0).getName());
-        assertEquals(token.getValue(), tokens.get(0).getValue());
+        assertOneToken("(2)", token);
     }
 
     @Test
     void analyzeLine_shouldNotRecognizeBrokenFloat() {
-        List<Token> tokens = lexer.analyzeLine("5.");
-        assertEquals(0, tokens.size());
+        assertZeroTokens("5.");
     }
 
     @Test
     void analyzeLine_shouldRecognizeNumberAddition() {
-        List<Token> tokens = lexer.analyzeLine("14 + 19");
         List<Token> expectedTokens = List.of(
             new Token("NUMBER", "14"), 
             new Token("OP", "+"),
             new Token("NUMBER", "19")
         );
-        assertEquals(3, tokens.size());
-        for (int i = 0; i < expectedTokens.size(); i++) {
-            assertEquals(expectedTokens.get(i).getName(), tokens.get(i).getName());
-            assertEquals(expectedTokens.get(i).getValue(), tokens.get(i).getValue());
-        }
+        assertManyTokens("14 + 19", expectedTokens);
     }
 
     @Test
     void analyzeLine_shouldRecognizeNumberIncrement() {
-        List<Token> tokens = lexer.analyzeLine("5++");
         List<Token> expectedTokens = List.of(
             new Token("NUMBER", "5"), 
             new Token("OP", "++")
         );
-        assertEquals(2, tokens.size());
-        for (int i = 0; i < expectedTokens.size(); i++) {
-            assertEquals(expectedTokens.get(i).getName(), tokens.get(i).getName());
-            assertEquals(expectedTokens.get(i).getValue(), tokens.get(i).getValue());
-        }
+        assertManyTokens("5++", expectedTokens);
     }
 
     @Test
     void analyzeLine_shouldRecognizeString() {
-        List<Token> tokens = lexer.analyzeLine("\"Hello\"");
         Token token = new Token("STRING", "\"Hello\"");
-        assertEquals(1, tokens.size());
-        assertEquals(token.getName(), tokens.get(0).getName());
-        assertEquals(token.getValue(), tokens.get(0).getValue());
+        assertOneToken("\"Hello\"", token);
     }
 
     @Test
     void analyzeLine_shouldNotRecognizeIncompleteString() {
-        List<Token> tokens = lexer.analyzeLine("\"Hello");
-        assertEquals(0, tokens.size());
+        assertZeroTokens("\"Hello");
     }
 
     @Test
     void analyzeLine_shouldRecognizeIdentifierAndInteger() {
-        List<Token> tokens = lexer.analyzeLine("print(2)");
         List<Token> expectedTokens = List.of(
             new Token("ID", "print"),
             new Token("NUMBER", "2")
         );
-        assertEquals(2, tokens.size());
-        for (int i = 0; i < expectedTokens.size(); i++) {
-            assertEquals(expectedTokens.get(i).getName(), tokens.get(i).getName());
-            assertEquals(expectedTokens.get(i).getValue(), tokens.get(i).getValue());
-        }
+        assertManyTokens("print(2)", expectedTokens);
     }
 
     @Test
     void analyzeLine_shouldRecognizeExpression() {
-        List<Token> tokens = lexer.analyzeLine("const float PI = 3.14");
         List<Token> expectedTokens = List.of(
             new Token("ID", "const"),
             new Token("ID", "float"),
@@ -110,16 +100,11 @@ public class TestLexer {
             new Token("OP", "="),
             new Token("NUMBER", "3.14")
         );
-        assertEquals(5, tokens.size());
-        for (int i = 0; i < expectedTokens.size(); i++) {
-            assertEquals(expectedTokens.get(i).getName(), tokens.get(i).getName());
-            assertEquals(expectedTokens.get(i).getValue(), tokens.get(i).getValue());
-        }
+        assertManyTokens("const float PI = 3.14", expectedTokens);
     }
 
     @Test 
     void analyzeLine_shouldRecognizeExpressionWithStrings() {
-        List<Token> tokens = lexer.analyzeLine("languages += \"Korean\" + \"Portuguese\"");
         List<Token> expectedTokens = List.of(
             new Token("ID", "languages"),
             new Token("OP", "+="),
@@ -127,24 +112,20 @@ public class TestLexer {
             new Token("OP", "+"),
             new Token("STRING", "\"Portuguese\"")
         );
-        assertEquals(5, tokens.size());
-        for (int i = 0; i < expectedTokens.size(); i++) {
-            assertEquals(expectedTokens.get(i).getName(), tokens.get(i).getName());
-            assertEquals(expectedTokens.get(i).getValue(), tokens.get(i).getValue());
-        }
+        assertManyTokens("languages += \"Korean\" + \"Portuguese\"", expectedTokens);
+    }
+
+    @Test
+    void analyzeLine_shouldIgnoreStandaloneInlineComment() {
+        assertZeroTokens("// this is a comment");
     }
 
     @Test
     void analyzeLine_shouldIgnoreInlineComment() {
-        List<Token> tokens = lexer.analyzeLine("y++ // y+= 1");
         List<Token> expectedTokens = List.of(
             new Token("ID", "y"),
             new Token("OP", "++")
         );
-        assertEquals(2, tokens.size());
-        for (int i = 0; i < expectedTokens.size(); i++) {
-            assertEquals(expectedTokens.get(i).getName(), tokens.get(i).getName());
-            assertEquals(expectedTokens.get(i).getValue(), tokens.get(i).getValue());
-        }
+        assertManyTokens("y++ // y+= 1", expectedTokens);
     }
 }
